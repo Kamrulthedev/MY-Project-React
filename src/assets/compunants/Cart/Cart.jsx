96c9136461cff97df9cbfb7bc6951903797546f1
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-19 17:15:45 +0600

    oky-commit

--- src/assets/compunants/Cart/Cart.jsx
+++ src/assets/compunants/Cart/Cart.jsx
@@ -8,6 +8,7 @@ const Cart = ({slcetedActor, remaing ,totleCost}) => {
     console.log(slcetedActor);
     return (
         <div >
+          <h1 className='font-bold'>KAMRUL</h1>
           <h1>totle actors: {slcetedActor.length}</h1>
           <h2>remaing: {remaing}</h2>
           <h3>Totle Cost:{totleCost}</h3>
